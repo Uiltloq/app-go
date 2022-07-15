86705f9473cfa259e0c7828c99ed5363c26e1bce
Uiltloq <uiltloq@gmail.com>
2022-07-15 18:58:32 +0300

    Test

--- tests/strings_test.go
+++ tests/strings_test.go
@@ -15,3 +15,30 @@ func TestMultiple(t *testing.T) {
 		t.Errorf("%d != %d", result, realResult)
 	}
 }
+
+func TestMulti(t *testing.T) {
+	t.Run("simple", func(t *testing.T) {
+		var x, y, result = 2, 2, 4
+		realResult := strings.Multiple(x, y)
+
+		if realResult != result {
+			t.Errorf("%d != %d", result, realResult)
+		}
+	})
+	t.Run("medium", func(t *testing.T) {
+		var x, y, result = 222, 222, 49284
+		realResult := strings.Multiple(x, y)
+
+		if realResult != result {
+			t.Errorf("%d != %d", result, realResult)
+		}
+	})
+	t.Run("negative", func(t *testing.T) {
+		var x, y, result = -2, -2, 4
+		realResult := strings.Multiple(x, y)
+
+		if realResult != result {
+			t.Errorf("%d != %d", result, realResult)
+		}
+	})
+}
