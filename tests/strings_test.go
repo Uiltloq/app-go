78561a0d6f6683a49e7cab4537bf797302542a96
Uiltloq <uiltloq@gmail.com>
2022-07-15 19:14:43 +0300

    Test group

--- tests/strings_test.go
+++ tests/strings_test.go
@@ -17,28 +17,36 @@ func TestMultiple(t *testing.T) {
 }
 
 func TestMulti(t *testing.T) {
-	t.Run("simple", func(t *testing.T) {
-		var x, y, result = 2, 2, 4
-		realResult := strings.Multiple(x, y)
+	t.Run("Group 1", func(t *testing.T) {
+		t.Run("simple", func(t *testing.T) {
+			t.Parallel()
+			t.Log("Parallel simple")
+			var x, y, result = 2, 2, 4
+			realResult := strings.Multiple(x, y)
 
-		if realResult != result {
-			t.Errorf("%d != %d", result, realResult)
-		}
-	})
-	t.Run("medium", func(t *testing.T) {
-		var x, y, result = 222, 222, 49284
-		realResult := strings.Multiple(x, y)
+			if realResult != result {
+				t.Errorf("%d != %d", result, realResult)
+			}
+		})
+		t.Run("medium", func(t *testing.T) {
+			t.Parallel()
+			t.Log("Parallel medium")
+			var x, y, result = 222, 222, 49284
+			realResult := strings.Multiple(x, y)
 
-		if realResult != result {
-			t.Errorf("%d != %d", result, realResult)
-		}
-	})
-	t.Run("negative", func(t *testing.T) {
-		var x, y, result = -2, -2, 4
-		realResult := strings.Multiple(x, y)
+			if realResult != result {
+				t.Errorf("%d != %d", result, realResult)
+			}
+		})
+		t.Run("negative", func(t *testing.T) {
+			t.Parallel()
+			t.Log("Parallel negative")
+			var x, y, result = -2, -2, 4
+			realResult := strings.Multiple(x, y)
 
-		if realResult != result {
-			t.Errorf("%d != %d", result, realResult)
-		}
+			if realResult != result {
+				t.Errorf("%d != %d", result, realResult)
+			}
+		})
 	})
 }
